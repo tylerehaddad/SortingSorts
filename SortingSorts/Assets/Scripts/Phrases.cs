fe46c3463361d24ddac6f76d1e68c843ff66fc4e
Tyler Haddad <ocarinasurge@gmail.com>
2016-10-24 23:27:23 -0400

    Unity Update and Phrase generation
    Updated to Unity 5.4.1f1, added Phrases script. Phrases contains a
    dynamic list that can be edited in editor to add all phrases to.
    Contains functions to generate strings of all the letters and one with
    underscores and spaces to generate the letter blocks and the spaces to
    place them.

---
+++ SortingSorts/Assets/Scripts/Phrases.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class Phrases : MonoBehaviour {
+
+    /// <summary>
+    /// List phrases contains the list of all phrases
+    /// to be used in the game. List is public
+    /// so that the list can be easily edited
+    /// in the Unity editor at any time.
+    /// The string currentPhrase is used to 
+    /// store the selected phrase for use
+    /// in generating spaces and letter blocks.
+    /// </summary>
+    
+    
+    public List<string> phrases;
+    private string currentPhrase;
+
+    /// <summary>
+    /// ChoosePhrase() is used to 
+    /// determine the phrase to be
+    /// used in generation of the 
+    /// spaces and letter blocks.
+    /// </summary>
+
+    void ChoosePhrase()
+    {
+        int i = Random.Range(0, phrases.Count);
+        currentPhrase = phrases[i];
+        Debug.Log(currentPhrase);
+    }
+
+    /// <summary>
+    /// Uses currentPhrase to generate a string
+    /// with no spaces and one with
+    /// only underscores and spaces. The former
+    /// is used to generate letter blocks, the
+    /// latter to generate underscores for
+    /// the letter blocks to attach to.
+    /// </summary>
+
+    void GenerateSpaces()
+    {
+        string phraseNoSpaces = currentPhrase.Replace(" ", "");
+        string phraseBlanks = "";
+
+        for (int i = 0; i < currentPhrase.Length - 1; i++)
+        {
+            if (currentPhrase[i] == ' ')
+            {
+                phraseBlanks += " ";
+            }
+            else
+            {
+                phraseBlanks += "_";
+            }
+        }
+
+        Debug.Log(phraseBlanks);
+    }
+
+    void Start()
+    {
+        ChoosePhrase();
+        GenerateSpaces();
+    }
+}
+
+
