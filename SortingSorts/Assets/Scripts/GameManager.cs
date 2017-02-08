3be20d8b5ba200c58108d68cbb0dd3e4dbcd1d3d
Yukon Wainczak <ywainczak@gmail.com>
2017-02-08 14:58:40 -0500

    Repaired the game a little.
    + I moved all the code over to a new object.
    + Cleaned up a bunch of stuff.

--- SortingSorts/Assets/Scripts/GameManager.cs
+++ SortingSorts/Assets/Scripts/GameManager.cs
@@ -105,8 +105,13 @@ public class GameManager : MonoBehaviour {
 	{
 		playing = true;
 
+		//Set the Variables.
+		score = 0;
+		roundTimer = roundTimerStart;
+
 		//Do the things.
 		ChoosePhrase ();
+		SortPhrase();
 	}
 
 	//OH BOI, WHAT PHRASE IS IT GOING TO THROW AT ME?!?
@@ -152,8 +157,223 @@ public class GameManager : MonoBehaviour {
 
 		for (int i = 0; i < currentPhrase.Length; i++)
 		{
-			
+			x -= blockWidth;
+
+			if (currentPhrase[i] != ' ')
+			{
+				GenerateBlocks(currentPhrase[i]);
+				Transform t = Instantiate(underscorePrefab).transform;
+				t.SetParent(stick);
+				if (x - blockWidth *(nextSpace - i) <= 60 - blockWidth * 9 && y == -26) 
+				{
+					while (x >= 60 - blockWidth * 10)
+					{
+						Transform empty = Instantiate(emptyBlockPrefab).transform;
+						empty.SetParent(stick);
+						empty.localPosition = new Vector2(x,y);
+						x -= blockWidth;
+					}
+
+					y = -14;
+					x = 60;
+				}
+				else if (x - blockWidth *(nextSpace - i) <= 60 - blockWidth * 9 && y == -14) 
+				{
+					while (x >= 60 - blockWidth * 10)
+					{
+						Transform empty = Instantiate(emptyBlockPrefab).transform;
+						empty.SetParent(stick);
+						empty.localPosition = new Vector2(x,y);
+						x -= blockWidth;
+					}
+					y = -2;
+					x = 60;
+				}
+
+				t.localPosition = new Vector2(x,y);
+
+				underscores.Add (t.GetComponent<Underscore>());
+			}
+			else
+			{
+				//Find Next Word Last I
+				for (int j = i+1; j < currentPhrase.Length; j++) 
+				{
+					if (currentPhrase [j] == ' ') 
+					{
+						nextSpace = j-1;
+						break;
+					}
+				}
+				if (nextSpace <= i)
+				{
+					nextSpace = currentPhrase.Length - 1;
+				}
+
+
+
+				Transform t = Instantiate(emptyBlockPrefab).transform;
+				t.SetParent(stick);
+				t.localPosition = new Vector2(x,y);
+
+			}
 		}
+	}
+
+	void GenerateBlocks(char c)
+	{
+		bool same;
+		float x;
+		float y = -15;
+		float offset = blockWidth - 1.5f;
+		int tries = 0;
+		int width = Mathf.Min(currentPhrase.Length /3, 7);
+		width = Mathf.Max (width, 5);
+		do
+		{
+			x = -width* offset + Random.Range (0, width*2) * offset;
+			same = false;
+
+			for (int i = 0; i < blocks.Count; i++) 
+			{
+				if (x == blocks[i].localPosition.x && y == blocks[i].localPosition.y) 
+				{
+					same = true;
+					y = -15;
+				}
+			}
+			if(same || x > width * offset + offset || x < -width * offset - offset)
+			{
+				if(y < -15)
+				{
+					same = true;
+					y = -15;
+					continue;
+				}
+				else
+				{
+					y = -30;
+					x = -width* offset + Random.Range (0, width*2) * offset;
+				}
+			}
+			same = false;
+			for (int i = 0; i < blocks.Count; i++) 
+			{
+				if (x == blocks[i].localPosition.x && y == blocks[i].localPosition.y) 
+				{
+					same = true;
+					y = -15;
+				}
+			}
+			tries++;
+			if(tries > 100)
+			{
+				same = false;
+			}
+		} while(same == true);
+
+
+		Letter l = Instantiate (blockPrefab).GetComponent<Letter> ();
+		l.transform.localPosition = new Vector2 (x, y);
+		l.letter = c;
+		l.AssignLetter();
+		blocks.Add (l.transform);
+	}
+
+	public Underscore getNextUnderscore()
+	{
+		if (underscores.Count >= 1) 
+		{
+			for (int i = 0; i < underscores.Count; i++) 
+			{
+				if (underscores [i].taken == false) 
+				{
+					return underscores [i];
+				}
+			}
+		}
+		return null;
+	}
+
+	//test if the phrase is correct
+	public void testPhrase()
+	{
+		string submitted = "";
+		string currentPhraseNoSpaces = "";
+		for (int i = 0; i < currentPhrase.Length; i++) 
+		{
+			if (currentPhrase [i] != ' ')
+			{
+				currentPhraseNoSpaces += currentPhrase [i];
+			}
+		}
+
+		for (int i = 0; i < underscores.Count; i++) 
+		{
+			if (underscores [i].transform.childCount >= 1)
+			{
+				if (underscores [i].transform.childCount > 1) 
+				{
+					Letter l = underscores [i].transform.GetComponentInChildren<Letter>();
+					submitted += l.letter;
+				}
+			}
+
+		}
+
+		if (submitted == currentPhraseNoSpaces) 
+		{
+			score += currentPhraseNoSpaces.Length;
+			scoreText.text = score.ToString ();
+			Reset ();
+		} 
+		else 
+		{
+			for (int i = 0; i < blocks.Count; i++) 
+			{
+				blocks [i].transform.SetParent (null);
+
+				blocks[i].position = blocks[i].GetComponent<Letter>().startPos;
+			}
+			for (int i = 0; i < underscores.Count; i++) 
+			{
+				underscores [i].taken = false;
+			}
+
+		}
+
+	}
+
+	void Reset()
+	{
+
+		for (int i = 0; i < blocks.Count; i++) 
+		{
+			Destroy (blocks [i].gameObject);
+		}
+
+		for (int i = 0; i < underscores.Count; i++) 
+		{
+			Destroy (underscores [i].gameObject);
+		}
+
+		foreach (GameObject g in GameObject.FindGameObjectsWithTag ("EmptyBlock"))
+		{
+			Destroy (g);
+		}
+
+		underscores = new List<Underscore> ();
+		blocks = new List<Transform> ();
+		ChoosePhrase();
+		SortPhrase();
+		roundTimer += 60f;
+	}
+
+	//Restart the game.
+	public void Restart()
+	{
+		Reset ();
+		Play ();
 	}
 
 	//I've had enough...
